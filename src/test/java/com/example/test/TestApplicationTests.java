3d9c6de91bf0bdfd32124fe969111c39d61b0d32
amycandoit <134021080+amycandoit@users.noreply.github.com>
2023-10-11 10:59:44 +0900

    Update TestApplicationTests.java

--- src/test/java/com/example/test/TestApplicationTests.java
+++ src/test/java/com/example/test/TestApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TestApplicationTests {
-
-    
     @Test
     void test() {
         Assertions.assertEquals(1+1, 2);
